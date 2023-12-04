d1ae656b8f14c83f5e78b4c14525370126b7f040
Alex Rosa <alexr@gec.inatel.br>
2023-12-04 16:37:14 -0300

    Class 14 - Criando interface - ServiceApi e endpoints

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,9 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+
+    const val PUBLIC_KEY = "0cd30e0ff90b6beb6e4f24628ba64226"
+    const val PRIVATE_KEY = "596b5651418e0083e81c92f19170d7c184f6b5e6\n"
+}

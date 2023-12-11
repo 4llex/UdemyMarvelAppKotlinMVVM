1106bd93ed70439dad5d3ccc47e59a5298b23d35
Alex Rosa <alexr@gec.inatel.br>
2023-12-11 17:25:54 -0300

    Class 28 - Implementando o fragment de detahles do character, DetailsCharacterFragment

--- app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailsCharacterFragment.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/details/DetailsCharacterFragment.kt
@@ -1,18 +1,100 @@
 package daniel.lop.io.marvelappstarter.ui.details
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterModel
 import daniel.lop.io.marvelappstarter.databinding.FragmentDetailsCharacterBinding
+import daniel.lop.io.marvelappstarter.ui.adapters.ComicAdapter
 import daniel.lop.io.marvelappstarter.ui.base.BaseFragment
+import daniel.lop.io.marvelappstarter.ui.state.ResourceState
+import daniel.lop.io.marvelappstarter.util.hide
+import daniel.lop.io.marvelappstarter.util.limitDescription
+import daniel.lop.io.marvelappstarter.util.show
+import daniel.lop.io.marvelappstarter.util.toast
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
 class DetailsCharacterFragment: BaseFragment<FragmentDetailsCharacterBinding, DetailsCharacterViewModel>() {
     override val viewModel: DetailsCharacterViewModel by viewModels()
 
+    private val comicAdapter by lazy { ComicAdapter() }
+    private lateinit var characterModel: CharacterModel
+    private val args: DetailsCharacterFragmentArgs by navArgs()
+
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentDetailsCharacterBinding = FragmentDetailsCharacterBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        characterModel = args.character
+        viewModel.fetch(characterModel.id)
+        setupRecyclerView()
+        onLoadCharacter(characterModel)
+        collectObserver()
+    }
+
+    private fun collectObserver() = lifecycleScope.launch {
+        viewModel.details.collect { result ->
+            when(result) {
+                is ResourceState.Success -> {
+                    binding.progressBarDetail.hide()
+                    result.data?.let { values ->
+                        if (values.data.results.count() > 0) {
+                            comicAdapter.comics = values.data.results.toList()
+                        } else {
+                            toast(getString(R.string.empty_list_comics))
+                        }
+                    }
+                }
+                is ResourceState.Error -> {
+                    binding.progressBarDetail.hide()
+                    result.message?.let {message ->
+                        Timber.tag("DetailsCharacterFragment").e("Error -> $message")
+                        toast(message)
+                    }
+                }
+                is ResourceState.Loading -> {
+                    binding.progressBarDetail.show()
+                }
+                else -> {}
+            }
+        }
+    }
+
+    private fun onLoadCharacter(characterModel: CharacterModel) = with(binding){
+        //Seta o nome do character na view
+        tvNameCharacterDetails.text = characterModel.name
+
+        //seta a descrição do character na view - se descrição esta vazia seta uma string default
+        if (characterModel.description.isEmpty()) {
+            tvDescriptionCharacterDetails.text = requireActivity().getString(R.string.text_description_empty)
+        } else {
+            tvDescriptionCharacterDetails.text = characterModel.description.limitDescription(100)
+        }
+
+        //seta a imagem do character utilizando a lib GLIDE
+        Glide.with(requireContext())
+            .load(characterModel.thumbnailModel.path +"."+characterModel.thumbnailModel.extension )
+            .into(imgCharacterDetails)
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        rvComics.adapter = comicAdapter
+        rvComics.layoutManager = LinearLayoutManager(context)
+    }
 }

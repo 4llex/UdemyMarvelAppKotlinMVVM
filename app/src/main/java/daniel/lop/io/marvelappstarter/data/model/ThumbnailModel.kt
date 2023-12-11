dbcfb303e45b7b1bb4fcf191b9482c762e8c7e4c
Alex Rosa <alexr@gec.inatel.br>
2023-12-11 19:47:53 -0300

    Class 28 - Fixing error: crash quando estava na tela de Detail e apertava home button

--- app/src/main/java/daniel/lop/io/marvelappstarter/data/model/ThumbnailModel.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/model/ThumbnailModel.kt
@@ -1,10 +1,11 @@
 package daniel.lop.io.marvelappstarter.data.model
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class ThumbnailModel(// represents: thumbnail/Image
     @SerializedName("path")
     val path: String,
     @SerializedName("extension")
     val extension: String
-)
+): Serializable
